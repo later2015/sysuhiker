30035547c2ea08ebb63b72893f54ca780c38f87c
later <448273866@qq.com>
2017-12-03 22:09:44 +0800

    更新签名说明

--- PhalApi/PhalApi/PhalApi/Filter/SysuhikerMD5.php
+++ PhalApi/PhalApi/PhalApi/Filter/SysuhikerMD5.php
@@ -1,20 +1,14 @@
 <?php
 /**
- * PhalApi_Filter_SimpleMD5 简单的MD5拦截器
  *
  * - 签名的方案如下：
  *
  * + 1、排除签名参数（默认是sign）
  * + 2、将剩下的全部参数，按参数名字进行字典排序
  * + 3、将排序好的参数，全部用字符串拼接起来
- * + 4、进行md5运算
+ * + 4、用当前日期拼接在3计算出来的字符串前，进行md5运算。
  *
  * 注意：无任何参数时，不作验签
- *
- * @package     PhalApi\Filter
- * @license     http://www.phalapi.net/license GPL 协议
- * @link        http://www.phalapi.net/
- * @author      dogstar <chanzonghuang@gmail.com> 2015-10-23
  */
 
 class PhalApi_Filter_SysuhikerMD5 implements PhalApi_Filter {
@@ -52,6 +46,7 @@ class PhalApi_Filter_SysuhikerMD5 implements PhalApi_Filter {
         $date=date('Ymd', time());
 //加上日期作为salt
         //https://www.phalapi.net/wikis/1-22.html
+        DI()->logger->debug('info:'.$date.$paramsStrExceptSign);
         return md5($date.$paramsStrExceptSign);
     }
 }
